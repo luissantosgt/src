dfb5b4c6e8073731b92d955a1baedd166eb50974
Luis Santos <alejandrogtsantos@gmail.com>
2024-10-09 18:11:01 -0600

    "practice bubble sort"

---
+++ Searching&Sorting/BubbleSort.js
@@ -0,0 +1,12 @@
+let arr = [1,4,2,5,-2,3]
+
+/* console.log(arr) */
+
+let i = 0
+let arrLenght = arr.length - 1
+let str = " "
+for(let iterator = 0; iterator <= arrLenght; iterator++){
+    str = str + iterator
+}
+
+console.log(str)
