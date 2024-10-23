284dbc30252526391de3ef065ba9f7c997f79a1b
ale <alejandrogtsantos@gmail.com>
2024-10-22 19:40:36 -0600

    "updates"

--- Searching&Sorting/BubbleSort.js
+++ Searching&Sorting/BubbleSort.js
@@ -1,12 +1,35 @@
-let arr = [1,4,2,5,-2,3]
+//https://www.geeksforgeeks.org/bubble-sort-algorithms-by-using-javascript/
 
-/* console.log(arr) */
-
-let i = 0
-let arrLenght = arr.length - 1
-let str = " "
-for(let iterator = 0; iterator <= arrLenght; iterator++){
-    str = str + iterator
+/* BubbleSort(array) {
+    for i -> 0 to arrayLength 
+        for j -> 0 to (arrayLength - i - 1)
+            if arr[j] > arr[j + 1]
+                swap(arr[j], arr[j + 1])
 }
+ */
 
-console.log(str)
+
+//let arr = [1,4,2,5,-2,3]
+//let arr = [];
+//console.log(arr);
+
+
+//console.log(arrLenght);
+//let greater = arr[1];
+
+function BubbleSort(arr){
+let arrLenght = arr.length
+for(let i = arrLenght; i > 0 ; i--){
+    for(let j = 0; j <= i - 1; j++){
+        if(arr[j] > arr[j + 1]){
+            var temp = arr[j]
+            arr[j] = arr[j + 1]
+            arr[j + 1] = temp
+        }
+    }
+}
+return arr;
+}
+//return arr;
+
+console.log(BubbleSort([1,4,2,5,-2,3]))
